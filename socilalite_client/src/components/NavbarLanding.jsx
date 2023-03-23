d22384b617509af6c0dc55e5c83cbebb408bb63b
RonexLemon <ronexondimu@gmail.com>
2023-03-23 15:22:47 +0300

    add landing navbar

---
+++ socilalite_client/src/components/NavbarLanding.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBarLanding = () =>{
+    return(
+        <div className="flex bg-blue-200 justify-around items-center  h-15   w-full">
+            <div></div>
+        
+            <div>
+                <button className="rounded-xl  h-8 w-20  bg-orange-400  text-white items-center">Start</button>
+
+            </div>
+
+
+        </div>
+    )
+
+}
+
+export default NavBarLanding;
