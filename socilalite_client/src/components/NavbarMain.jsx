e5a1e26f2748a96578c6cc4fcbb0cea14fd22b3b
RonexLemon <ronexondimu@gmail.com>
2023-03-23 22:08:34 +0300

    modify sidebar

--- socilalite_client/src/components/NavbarMain.jsx
+++ socilalite_client/src/components/NavbarMain.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 const NavBarMain = () =>{
     const navigate = useNavigate();
     return(
-        <div className="flex bg-red-400 justify-around items-center  h-15   w-full">
-            <div></div>
+        <div className="flex bg-slate-400 justify-around items-center  h-15   w-full">
+            <div>
+                <span>
+                    Home
+                </span>
+            </div>
         
             <div>
-                <button   onClick={()=>{navigate("/main")}} className="rounded-xl  h-8 w-20  bg-orange-400  text-white items-center">Start</button>
+                <button    className="rounded-xl  h-8 w-20  bg-green-700  text-white items-center">Connected</button>
 
             </div>
 
