a9efe814bbd11b52856e510e13812a27e5e71253
RonexLemon <ronexondimu@gmail.com>
2023-03-23 15:22:17 +0300

    implement landing page

---
+++ socilalite_client/src/components/LandingPage/Land.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NavBarLanding from "../NavbarLanding";
+const LandingPage = () =>{
+    return(
+        <div className="bg-background h-screen ">
+            <NavBarLanding/>
+        </div>
+        
+    )
+   
+    
+
+}
+export default LandingPage;
