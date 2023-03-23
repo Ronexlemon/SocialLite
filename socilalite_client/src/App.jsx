96b2da01d2a4214064580b4f0379003a85186e35
RonexLemon <ronexondimu@gmail.com>
2023-03-23 20:58:17 +0300

    implement routes

--- socilalite_client/src/App.jsx
+++ socilalite_client/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LandingPage from './components/LandingPage/Land'
+import LandPage from './Pages/LandingPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=" h-screen w-full">
-      <LandingPage/>
-     </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandPage/>} />
+        <Route path="/home" element={<LandPage/>} />
+        
+      </Routes>
+    </Router>
   )
 }
 
--- socilalite_client/src/Pages/Home.jsx
+++ socilalite_client/src/Pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Home = ()=>{
+return(
+    <div className="bg-blue-200 h-screen ">
+        
 
+    </div>
+)
 }
 export default Home;
