897020e6202419d954493b571bacbf21c674cd5b
RonexLemon <ronexondimu@gmail.com>
2023-03-24 23:22:38 +0300

    connect to metamask wallet

--- socilalite_client/src/App.jsx
+++ socilalite_client/src/App.jsx
@@ -1,17 +1,58 @@
-import { useState } from 'react'
+import { useState,useRef,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandPage from './Pages/LandingPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Web3Modal from "web3modal"
+import {providers,Contract} from "ethers"
 import Main from './Pages/Main'
 import SinglePostComment from './Pages/SinglePostComment'
 import Post from './Pages/Post'
 import TrendingPosts from './Pages/Trending'
+import { AppContext } from '../contexts/AppContexts';
+//import { providerOptions } from "xdcpay-connect";
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const web3ModalRef = useRef();
+  const getProviderOrSigner = async (needSigner = false)=>{
+    try{
+        const provider = await web3ModalRef.current.connect();
+        const web3Provider = new providers.Web3Provider(provider);
+        const {chainId}  = await web3Provider.getNetwork();
+        console.log("the chain",chainId);
+        if(chainId !=51){
+            alert("please connect to The XDC Apothem Network")
+        }
+        
+        if(needSigner){
+            const signer = web3Provider.getSigner();
+            return signer;
+        }
+        return web3Provider;
+
+    }catch(error){
+        console.log(error);
+    }
+}
+useEffect(()=>{
+web3ModalRef.current =new Web3Modal({
+    network: "xinfin",
+    providerOptions:{},
+    disableInjectedProvider: false,
+    cacheProvider: false,
+  });
+getProviderOrSigner();
+},[])
 
   return (
+    <AppContext.Provider value={{
+      getProviderOrSigner,
+      
+      Contract,
+      
+    }}>
     <Router>
       <Routes>
         <Route path="/" element={<LandPage/>} />
@@ -23,6 +64,7 @@ function App() {
         
       </Routes>
     </Router>
+    </AppContext.Provider>
   )
 }
 
